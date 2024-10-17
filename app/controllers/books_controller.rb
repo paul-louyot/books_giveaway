1fa424b2ddadf480c53015a714ade174d645ec4d
Paul Louyot <paul@Pauls-MacBook-Pro.local>
2024-10-17 18:28:55 +0200

    feat: update UI on book reservation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
   http_basic_authenticate_with name: "paul", password: "secret", except: [ :index, :update ]
+  before_action :set_user
 
   def index
     @books = Book.all
-    @user_name = cookies[:user_name]
   end
 
   def create
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find_by(id: params[:id])
     book.update!(book_params)
+    render partial: "form", locals: { book: book, user_name: @user_name }
   end
 
   def new
@@ -25,5 +26,9 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:user_name)
+  end
+
+  def set_user
+    @user_name = cookies[:user_name]
   end
 end
