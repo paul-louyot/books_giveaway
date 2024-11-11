406a7772dbefac7001b79f05826ee2960116ae66
Paul Louyot <paul@Pauls-MacBook-Pro.local>
2024-11-11 12:38:55 +0100

    Setup daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    './app/views/**/*.html.haml',
+    './public/*.html',
     './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    container: {
+      padding: '2rem',
+      center: true,
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require('daisyui'),
   ]
 }
